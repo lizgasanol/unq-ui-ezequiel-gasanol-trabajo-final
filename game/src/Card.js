b7f90f7eb7640ebd0e9f022e2cf11e8743b62376
Ezequiel Gasañol <ezequiel.a.gasanol@gmail.com>
2020-12-07 20:47:37 -0300

    lógica básica para poder jugar contra la pc

--- game/src/Card.js
+++ game/src/Card.js
@@ -2,27 +2,7 @@ import React from "react"
 
 const Card = (props) => {
     const value = props.value;
-
-    const winsAgainst = (aValue, opponentValue) => {
-        if (aValue === 5) {
-            return opponentValue === 1 || opponentValue === 3
-        } else {
-            return opponentValue === shiftValue(aValue,1) || opponentValue === shiftValue(aValue,3)
-        }
-    };
-
-    const shiftValue = (aValue, numberToShift) => {
-        let ret = aValue
-        for(let i = numberToShift; i !== 0; i--){
-            if(ret === 5) {
-                ret = 1;
-            } else {
-                ret++;
-            }
-        }
-        return ret;
-    };
-
+    
     return(
         <>
             <div className="card" onClick = {() => {props.onClick(value)}}>
--- game/src/Game.js
+++ game/src/Game.js
@@ -10,11 +10,19 @@ const Game = () => {
 
     const handleStatusChange = (value) => {
         setGameState({
-            ...gameState,
-            playerChoice: value
+            playerChoice: value,
+            cpuChoice: getRandomChoice(),
+            points: sumPointIfApplicable()
         })
-        console.log(gameState.playerChoice)
     };
+
+    const sumPointIfApplicable = () => {
+        let ret = gameState.points;
+        if (winsAgainst(gameState.playerChoice,gameState.cpuChoice)) {
+            ret++
+        }
+        return ret;
+    }
 
     useEffect(() => {
         setGameState({
@@ -24,38 +32,91 @@ const Game = () => {
         })
     },[])
 
+    const getPoints = () => {
+        return gameState.points
+    }
+
+    const getRandomChoice = () => {
+        return Math.floor(Math.random() * 5) + 1
+    }
+
+    const translateChoice = (x) => {
+        switch (x){
+            case 1:
+                return "Scissors";
+            case 2:
+                return "Paper";
+            case 3: 
+                return "Rock";
+            case 4:
+                return "Lizard";
+            case 5:
+                return "Spock";
+            default:
+                return "Not chosen"
+        }
+    }
+
+    const isTie = () => {
+        return gameState.playerChoice === gameState.cpuChoice
+    }
+
+    const winsAgainst = (aValue, opponentValue) => {
+            return opponentValue === shiftValue(aValue,1) || opponentValue === shiftValue(aValue,3)
+ 
+    };
+
+    const shiftValue = (aValue, numberToShift) => {
+        let ret = aValue
+        for(let i = numberToShift; i !== 0; i--){
+            if(ret === 5) {
+                ret = 1;
+            } else {
+                ret++;
+            }
+        }
+        return ret;
+    };
     return(
         <>
             <img src={instrucciones} alt="Instrucciones"/>
             <h1>Pick a card</h1>
-            points: {gameState.points}
+            Victories: {sumPointIfApplicable()/* si no hago esto los puntos no se actualizan, no se por qué */ } 
             <div className="cardwrapper">
                 <Card name="Scissors"
                 img="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/57f1632d7303fbcd8974425882ffd9919fc4041b/svgs/regular/hand-scissors.svg"
                 description="Cuts paper. Decapitates lizard"
-                value="1"
+                value={1}
                 onClick={handleStatusChange}/>
                 <Card name="Paper"
                 img="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/57f1632d7303fbcd8974425882ffd9919fc4041b/svgs/regular/hand-paper.svg"
                 description="Covers rock. Deauthorizes Spock"
-                value="2"
+                value={2}
                 onClick={handleStatusChange}/>
                 <Card name="Rock"
                 img="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/57f1632d7303fbcd8974425882ffd9919fc4041b/svgs/regular/hand-rock.svg"
                 description="Smashes lizard and scissors"
-                value="3"
+                value={3}
                 onClick={handleStatusChange}/>
                 <Card name="Lizard" 
                 img="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/57f1632d7303fbcd8974425882ffd9919fc4041b/svgs/regular/hand-lizard.svg" 
                 description="Poisons Spock. Devours paper."
-                value="4"
+                value={4}
                 onClick={handleStatusChange}/>
                 <Card name="Spock"
                 img="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/57f1632d7303fbcd8974425882ffd9919fc4041b/svgs/regular/hand-spock.svg"
                 description="Breaks scissors. Vaporizes rock"
-                value="5"
+                value={5}
                 onClick={handleStatusChange}/>
             </div>
+            {gameState.cpuChoice !== undefined && <>
+            {winsAgainst(gameState.playerChoice,gameState.cpuChoice) && <h1>You win!</h1>}
+            {winsAgainst(gameState.cpuChoice,gameState.playerChoice) && <h1>Opponent wins!</h1>}
+            {isTie() && <h1>It's a tie!</h1>}
+            <h1>your choice: {translateChoice(gameState.playerChoice)}</h1>
+            <h1>opponent choice: {translateChoice(gameState.cpuChoice)}</h1>
+            <h2>To play again pick another card.</h2>
+            </>}
         </>
     );
 }
